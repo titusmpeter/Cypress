9d1ec1a85fcab10cf7943b956494b7280b325de2
Peter Titus <peter.titus@detegoglobal.com>
2026-08-06 17:09:41 +0100

    Sibling elements

--- examples/Test6.js
+++ examples/Test6.js
@@ -1,7 +1,30 @@
+/// <reference types='Cypress' />
+//Cypress has the ability to manipulate DOM
+
 describe("Test Suite VI", ()=>{
 
     it("Web Tables",()=>{
         cy.visit("https://rahulshettyacademy.com/automationpractise/#/");
+
+        //finding the nth child - find the 2nd item in a row, iterate through row's 2nd item
+        cy.get("#product tr td:nth-child(2)").each(($el, index, $list) => {
+
+            const rowText=$el.text();
+
+            if (rowText.includes("Python Language")) {
+
+                //move to the sibling element
+                cy.get("tr td:nth-child(2)").eq(index).next().then( (price) => {
+
+                    //get text from sibling element
+                    const priceText = price.text();
+                    expect(priceText).to.equal("25"); //validate
+                })
+
+            }
+
+        })
+
 
     })
 
