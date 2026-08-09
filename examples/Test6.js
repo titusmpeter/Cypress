93692c6497fbbbbccc21f30417b76833e27ab2b6
Peter Titus <titusmpeter@gmail.com>
2026-08-09 17:13:58 +0100

    untracked

--- examples/Test6.js
+++ examples/Test6.js
@@ -1,32 +1,25 @@
 /// <reference types='Cypress' />
 //Cypress has the ability to manipulate DOM
 
-describe("Test Suite VI", ()=>{
+describe("Test Suite VI", () => {
+  it("Web Tables", () => {
+    cy.visit("https://rahulshettyacademy.com/automationpractise/#/");
 
-    it("Web Tables",()=>{
-        cy.visit("https://rahulshettyacademy.com/automationpractise/#/");
+    //finding the nth child - find the 2nd item in a row, iterate through row's 2nd item
+    cy.get("#product tr td:nth-child(2)").each(($el, index, $list) => {
+      const rowText = $el.text();
 
-        //finding the nth child - find the 2nd item in a row, iterate through row's 2nd item
-        cy.get("#product tr td:nth-child(2)").each(($el, index, $list) => {
-
-            const rowText=$el.text();
-
-            if (rowText.includes("Python Language")) {
-
-                //move to the sibling element
-                cy.get("tr td:nth-child(2)").eq(index).next().then( (price) => {
-
-                    //get text from sibling element
-                    const priceText = price.text();
-                    expect(priceText).to.equal("25"); //validate
-                })
-
-            }
-
-        })
-
-
-    })
-
-
-})
+      if (rowText.includes("Python Language")) {
+        //move to the sibling element
+        cy.get("tr td:nth-child(2)")
+          .eq(index)
+          .next()
+          .then((price) => {
+            //get text from sibling element
+            const priceText = price.text();
+            expect(priceText).to.equal("25"); //validate
+          });
+      }
+    });
+  });
+});
