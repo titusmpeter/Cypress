46b40f002afeaf1bae83ff64d65444b3d38d064d
Peter Titus <peter.titus@detegoglobal.com>
2026-08-21 17:21:44 +0100

    comments

--- examples/Test8.js
+++ examples/Test8.js
@@ -8,12 +8,13 @@ import "cypress-iframe";
 describe("TS 8", () => {
   //HTML Document embedded within another document
   it("Embedded Frames", () => {
+    //locate frame
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.frameLoaded("#courses-iframe");
-    //locate frame
-    cy.iframe().find("a[href*='mentorship']").eq(0).click();
     //switch to iframe mode
     //click first element in the array i.e. (0)
+    cy.iframe().find("a[href*='mentorship']").eq(0).click();
+    //assertion - validation
     cy.iframe().find("h1[class*='pricing-title']").should("have.length", 2);
   });
 });
